35f78ce1a56a0df142a0c09171dbb5b57ecf2a7b
川端洋平 <bata44883@gmail.com>
2018-04-06 16:28:15 +0900

    テンプレート除去

--- app/controllers/system/products_controller.rb
+++ app/controllers/system/products_controller.rb
@@ -2,7 +2,7 @@ class System::ProductsController < System::ApplicationController
   def index
     @date = params[:date] ? Date.new(params[:date][:year].to_i, params[:date][:month].to_i, 1) : Time.now
 
-    @products  = Product.includes(:product_images, :user).where(created_at: @date.beginning_of_month..@date.end_of_month).order(created_at: :desc)
-    @pproducts = @products.page(params[:page]).per(200)
+    @products  = Product.includes(:product_images, :user).where(created_at: @date.beginning_of_month..@date.end_of_month, template: false).order(created_at: :desc)
+    @pproducts = @products.page(params[:page]).per(100)
   end
 end
