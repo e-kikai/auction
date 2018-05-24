07e6eac6a85ebc2bbdddea27feb9850f737c5da9
川端洋平 <bata44883@gmail.com>
2018-05-24 18:27:34 +0900

    落札履歴修正

--- app/controllers/system/bids_controller.rb
+++ app/controllers/system/bids_controller.rb
@@ -1,8 +1,17 @@
 class System::BidsController < System::ApplicationController
   def index
     @date = params[:date] ? Date.new(params[:date][:year].to_i, params[:date][:month].to_i, params[:date][:day].to_i) : Time.now
+    @company = params[:company]
 
-    @bids  = Bid.includes(:product, :user).where(created_at: @date.beginning_of_day..@date.end_of_day).order(created_at: :desc)
+    @bids  = Bid.includes(:product, :user).where(created_at: @date.all_day).order(created_at: :desc)
+
+    if @company.present?
+      product_ids = User.find(@company).products.select(:id)
+      @bids = @bids.where(product_id: product_ids)
+    end
+
     @pbids = @bids.page(params[:page]).per(500)
+
+    @company_selectors = User.companies.order(:id).map { |co| [co.company_remove_kabu, co.id] }
   end
 end
--- app/controllers/system/products_controller.rb
+++ app/controllers/system/products_controller.rb
@@ -3,7 +3,7 @@ class System::ProductsController < System::ApplicationController
     @date    = params[:date] ? Date.new(params[:date][:year].to_i, params[:date][:month].to_i, 1) : Time.now
     @company = params[:company]
 
-    @products  = Product.includes(:product_images, :user).where(created_at: @date.beginning_of_month..@date.end_of_month, template: false).order(created_at: :desc)
+    @products  = Product.includes(:product_images, :user).where(created_at: @date.all_month, template: false).order(created_at: :desc)
 
     @products = @products.where(user: @company) if @company.present?
 
@@ -16,7 +16,7 @@ class System::ProductsController < System::ApplicationController
     @date    = params[:date] ? Date.new(params[:date][:year].to_i, params[:date][:month].to_i, params[:date][:day].to_i) : Time.now
     @company = params[:company]
 
-    @products  = Product.includes(:product_images, :user).where(dulation_end: @date.beginning_of_day..@date.end_of_day, template: false, cancel: nil).where.not(max_bid_id: nil).order(created_at: :desc)
+    @products  = Product.status(params[:cond]).includes(:product_images, :user).where(dulation_end: @date.all_day, template: false, cancel: nil).where.not(max_bid_id: nil).order(dulation_end: :desc)
 
     @products = @products.where(user: @company) if @company.present?
 
