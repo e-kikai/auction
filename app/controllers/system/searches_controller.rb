3baf4a23b2282b4bf9da312a8086e7c01175f623
川端洋平 <bata44883@gmail.com>
2018-06-11 12:29:47 +0900

    保存した検索条件履歴利用数

--- app/controllers/system/searches_controller.rb
+++ app/controllers/system/searches_controller.rb
@@ -2,5 +2,8 @@ class System::SearchesController < System::ApplicationController
   def index
     @searches = Search.all.includes(:user, :category, :company).order(created_at: :desc)
     @psearches = @searches.page(params[:page]).per(500)
+
+    @search_log_counts = SearchLog.group(:search_id).where(search_id: @searches.select(:id)).count
+
   end
 end
