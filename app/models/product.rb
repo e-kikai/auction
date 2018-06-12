22dec32977a05473725b38c5ccc87ea60ea822a2
川端洋平 <bata44883@gmail.com>
2018-06-13 02:17:21 +0900

    人気商品8

--- app/models/product.rb
+++ app/models/product.rb
@@ -159,11 +159,13 @@ class Product < ApplicationRecord
   }
 
   scope :populars, -> products {
-    res = Product.status(STATUS[:start]).includes(:product_images).reorder("((bids_count + 1) * (watches_count + 1)) DESC", :dulation_end)
+    res = Product.status(STATUS[:start]).includes(:product_images)
+      .except(:order).order("((bids_count + 1) * (watches_count + 1)) DESC", :dulation_end)
+
     if products.class.name == "Product"
       res.where(category_id: products.category.subtree_ids).where.not(id: products.id)
     else
-      res.where(category_id: products.select(:category_id)).where.not(id: products.select(:id))
+      res.where(category_id: products.except(:order).select(:category_id)).where.not(id: products.except(:order).select(:id))
     end
 
   }
