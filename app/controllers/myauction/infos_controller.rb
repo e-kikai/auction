818db259b276dfbce2d29f3891bf3737009507c2
川端洋平 <bata44883@gmail.com>
2018-06-19 16:26:46 +0900

    お知らせリンク修正

--- app/controllers/myauction/infos_controller.rb
+++ app/controllers/myauction/infos_controller.rb
@@ -6,6 +6,6 @@ class Myauction::InfosController < Myauction::ApplicationController
   def show
     @infos = Info.where(target: 100).where("start_at <= ?", Time.now).order(:start_at)
 
-    @info  = Info.where(target: 100).find(params[:id])
+    @info  = Info.where(target: 100).find_by(uid: params[:id])
   end
 end
