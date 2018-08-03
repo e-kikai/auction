99771f50c99f7fa172d6c1d5f17aa4b435e3268f
川端洋平 <bata44883@gmail.com>
2018-08-03 17:39:04 +0900

    新着メール通知履歴3

--- app/controllers/system/alerts_controller.rb
+++ app/controllers/system/alerts_controller.rb
@@ -1,6 +1,6 @@
 class System::AlertsController < System::ApplicationController
   def index
-    @alerts = Search.unscoped.all.includes(:user, :category, :company).order(created_at: :desc)
+    @alerts = Alerts.unscoped.all.includes(:user, :category, :company).order(created_at: :desc)
     @palerts = @alerts.page(params[:page]).per(500)
   end
 end
