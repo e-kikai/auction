c41a7c317b600654fb46b407612496591fb996c0
川端洋平 <bata44883@gmail.com>
2018-10-24 15:28:41 +0900

    履歴を月単位に変更

--- app/controllers/system/detail_logs_controller.rb
+++ app/controllers/system/detail_logs_controller.rb
@@ -2,9 +2,11 @@ class System::DetailLogsController < System::ApplicationController
   include Exports
 
   def index
-    @date = params[:date] ? Date.new(params[:date][:year].to_i, params[:date][:month].to_i, params[:date][:day].to_i) : Time.now
+    # @date = params[:date] ? Date.new(params[:date][:year].to_i, params[:date][:month].to_i, params[:date][:day].to_i) : Time.now
+    # @detail_logs  = DetailLog.includes(:product, :user).where(created_at: @date.beginning_of_day..@date.end_of_day).order(created_at: :desc)
 
-    @detail_logs  = DetailLog.includes(:product, :user).where(created_at: @date.beginning_of_day..@date.end_of_day).order(created_at: :desc)
+    @date = params[:date] ? Time.new(params[:date][:year].to_i, params[:date][:month].to_i, 1) : Time.now
+    @detail_logs  = DetailLog.includes(:product, :user).where(created_at: @date.beginning_of_month..@date.end_of_month).order(created_at: :desc)
 
 
     respond_to do |format|
--- app/controllers/system/search_logs_controller.rb
+++ app/controllers/system/search_logs_controller.rb
@@ -1,8 +1,11 @@
 class System::SearchLogsController < System::ApplicationController
   def index
-    @date = params[:date] ? Date.new(params[:date][:year].to_i, params[:date][:month].to_i, params[:date][:day].to_i) : Time.now
+    # @date = params[:date] ? Date.new(params[:date][:year].to_i, params[:date][:month].to_i, params[:date][:day].to_i) : Time.now
+    # @search_logs  = SearchLog.includes(:user, :category, :company).where(created_at: @date.beginning_of_day..@date.end_of_day).order(created_at: :desc)
 
-    @search_logs  = SearchLog.includes(:user, :category, :company).where(created_at: @date.beginning_of_day..@date.end_of_day).order(created_at: :desc)
+    @date = params[:date] ? Time.new(params[:date][:year].to_i, params[:date][:month].to_i, 1) : Time.now
+    @search_logs  = SearchLog.includes(:user, :category, :company).where(created_at: @date.beginning_of_month..@date.end_of_month).order(created_at: :desc)
+
     @psearch_logs = @search_logs.page(params[:page]).per(500)
   end
 end
