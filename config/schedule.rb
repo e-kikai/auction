97aa573db558b4ae65f23e7e24376dcb5d40c6e5
川端洋平 <bata44883@gmail.com>
2019-06-30 23:40:38 +0900

    intervalの修正2

--- config/schedule.rb
+++ config/schedule.rb
@@ -41,7 +41,7 @@ end
 # Twitter自動投稿
 if rails_env.to_sym == :production
   # every :day, at: ['6:00 am', '6:00 pm'] do
-  every(Product::TWITTER_INTERVAL) do
+  every 6.hours do
     # rake 'twitter:new_product'
     command "wget --spider #{scheduling_url + '/twitter_new_product'}"
   end
