e8e1b3de6dc2e2a29058ad78a4a1318504fbcbc4
川端洋平 <bata44883@gmail.com>
2020-05-27 01:39:50 +0900

    まとめ取得修正

--- app/controllers/system/total_controller.rb
+++ app/controllers/system/total_controller.rb
@@ -1,32 +1,26 @@
 class System::TotalController < System::ApplicationController
   include Exports
 
-   before_action :company_selectors
+   before_action :company_selectors, exsist: [:index]
+   before_action :date_selectors,    exsist: [:products_monthly]
 
   def index
-    @date = params[:date] ? Time.new(params[:date][:year].to_i, params[:date][:month].to_i, 1) : Time.now
-
     @companies = User.companies.order(:id)
 
     respond_to do |format|
       format.html
       format.pdf {
-        send_data render_to_string,
+        send_data @render_to_string,
           filename:     "total_#{@date.strftime('%Y%m')}.pdf",
           content_type: "application/pdf",
           disposition:  "inline"
       }
-      format.csv { export_csv "total.csv" }
+      format.csv { export_csv "total_#{@date.strftime('%Y%m')}.csv" }
     end
   end
 
   def products
-    @date    = params[:date] ? Date.new(params[:date][:year].to_i, params[:date][:month].to_i, 1) : Time.now.to_date
-    @company = params[:company]
-
     # 取得
-    rstart = @date.beginning_of_month
-    rend   = @date.end_of_month
     auto_sql = "DATE(dulation_end) + auto_resale * auto_resale_date"
     # @products  = Product.includes(:user).where(created_at: @date.beginning_of_month..@date.end_of_month, template: false)
     @products = Product.includes(:user).where(template: false)
@@ -34,58 +28,34 @@ class System::TotalController < System::ApplicationController
 
     gro       = "DATE(created_at)"
     gro_end   = "DATE(dulation_end)"
-    where_cr  = ["DATE(created_at) BETWEEN ? AND ?", rstart, rend]
-    where_str = ["DATE(dulation_start) BETWEEN ? AND ?", rstart, rend]
-    where_end = ["DATE(dulation_end) BETWEEN ? AND ?", rstart, rend]
 
     # 集計
-    @start_counts   = @products.group("DATE(dulation_start)").where(where_str).count
-    @end_counts     = @products.where(cancel: nil, max_bid_id: nil).group(gro_end).where(where_end).count
-    @fin_counts     = @products.where(cancel: nil, max_bid_id: nil).group(auto_sql).having("#{auto_sql} BETWEEN ? AND ?", rstart, rend).count
-    @cancel_counts  = @products.where.not(cancel: nil).where(max_bid_id: nil).group(gro_end).where(where_end).count
-    @success        = @products.where(cancel: nil).where.not(max_bid_id: nil).group(gro_end).where(where_end)
+    @start_counts   = @products.group("DATE(dulation_start)").where(@where_str).count
+    @end_counts     = @products.where(cancel: nil, max_bid_id: nil).group(gro_end).where(@where_end).count
+    @fin_counts     = @products.where(cancel: nil, max_bid_id: nil).group(auto_sql).having("#{auto_sql} BETWEEN ? AND ?", @rstart, @rend).count
+    @cancel_counts  = @products.where.not(cancel: nil).where(max_bid_id: nil).group(gro_end).where(@where_end).count
+    @success        = @products.where(cancel: nil).where.not(max_bid_id: nil).group(gro_end).where(@where_end)
     @success_counts = @success.count
     @success_prices = @success.sum(:max_price)
 
-    # if @company.present?
-    #   product_ids = @products.select(:id)
-    #
-    #   bids        = Bid.where(product_id: product_ids)
-    #   detail_logs = DetailLog.where(product_id: product_ids)
-    #   watches     = Watch.where(product_id: product_ids)
-    # else
-    #   bids        = Bid.all
-    #   detail_logs = DetailLog.all
-    #   watches     = Watch.all
-    # end
-
     # 入札・ユーザ集計
-    @bid_counts          = Bid.where(product_id: @products).where(where_cr).group(gro).count
-    @bid_user_counts     = Bid.where(product_id: @products).where(where_cr).group(gro).distinct.count(:user_id)
-    @bid_user_total      = Bid.where(product_id: @products).where(where_cr).distinct.count(:user_id)
-    @detail_log_counts   = DetailLog.where(product_id: @products).where(where_cr).group(gro).count
-    @detail_user_counts  = DetailLog.where(product_id: @products).where(where_cr).group(gro).distinct.count(:ip)
-    @detail_user_total   = DetailLog.where(product_id: @products).where(where_cr).distinct.count(:ip)
-    @watch_counts        = Watch.where(product_id: @products).where(where_cr).group(gro).count
-    @watch_user_counts   = Watch.where(product_id: @products).where(where_cr).group(gro).distinct.count(:user_id)
-    @watch_user_total    = Watch.where(product_id: @products).where(where_cr).distinct.count(:user_id)
-    @user_counts         = User.group(gro).where(where_cr).count
+    @bid_counts          = Bid.where(product_id: @products).where(@where_cr).group(gro).count
+    @bid_user_counts     = Bid.where(product_id: @products).where(@where_cr).group(gro).distinct.count(:user_id)
+    @bid_user_total      = Bid.where(product_id: @products).where(@where_cr).distinct.count(:user_id)
+    @detail_log_counts   = DetailLog.where(product_id: @products).where(@where_cr).group(gro).count
+    @detail_user_counts  = DetailLog.where(product_id: @products).where(@where_cr).group(gro).distinct.count(:ip)
+    @detail_user_total   = DetailLog.where(product_id: @products).where(@where_cr).distinct.count(:ip)
+    @watch_counts        = Watch.where(product_id: @products).where(@where_cr).group(gro).count
+    @watch_user_counts   = Watch.where(product_id: @products).where(@where_cr).group(gro).distinct.count(:user_id)
+    @watch_user_total    = Watch.where(product_id: @products).where(@where_cr).distinct.count(:user_id)
+    @user_counts         = User.group(gro).where(@where_cr).count
 
-    # @bid_counts         = bids.group("DATE(created_at)").having("DATE(created_at) BETWEEN ? AND ?", rstart, rend).count
-    # @detail_log_counts  = detail_logs.group("DATE(created_at)").having("DATE(created_at) BETWEEN ? AND ?", rstart, rend).count
-    # @watch_counts       = watches.group("DATE(created_at)").having("DATE(created_at) BETWEEN ? AND ?", rstart, rend).count
-    #
-    # @user_counts        = User.group("DATE(created_at)").having("DATE(created_at) BETWEEN ? AND ?", rstart, rend).count
-
-    @start_count        = @products.where("dulation_start <= ? AND dulation_end > ?", rstart, rstart).count
+    @start_count        = @products.where("dulation_start <= ? AND dulation_end > ?", @rstart, @rstart).count
   end
 
   def products_monthly
-    @company = params[:company]
-
     @products = Product.includes(:user).where(template: false)
     @products = @products.where(user: @company) if @company.present? # 会社別
-
 
     endd = Product.maximum(:dulation_end)
     @monthes = (Date.new(2018, 3, 1) .. endd).select{|date| date.day == 1}.map { |d| d.strftime('%Y/%m')}
@@ -115,34 +85,56 @@ class System::TotalController < System::ApplicationController
   end
 
   def formula
-    @date    = params[:date] ? Time.new(params[:date][:year].to_i, params[:date][:month].to_i, 1) : Time.now
-    @company = params[:company]
-
-    # 取得
-    rstart = @date.beginning_of_month
-    rend   = @date.end_of_month
-
     # 期間内に出品されていた商品
-    @products = Product.includes(:user, :max_bid).where(template: false, cancel: nil).where("dulation_start < ? AND dulation_end > ?", rend, rstart)
+    @products = Product.includes(:user).where(template: false)
 
-    @success_products = @products.where.not(max_bid_id: nil).where("dulation_end < ? ", rend)
-    @max_product      = @success_products.order("max_price DESC").first
-    @max_bid_product  = @success_products.order("bids_count DESC").first
+    @products = Product.includes(:user, :max_bid).where(template: false, cancel: nil).where("dulation_start < ? AND dulation_end > ?", @rend, @rstart)
 
-    @day = rend.day
+    @now_count = @products.where("dulation_start < ? AND dulation_end > ?", @rend, @rstart).count
 
-    @users = User.where(created_at: rstart..rend)
+    @success       = @products.where(cancel: nil).where.not(max_bid_id: nil).where(@where_end)
+    @success_count = @success.count
+    @success_price = @success.sum(:max_price)
 
-    @bids = Bid.where(product_id:  @success_products)
+    @success_products = @products.where.not(max_bid_id: nil).where("dulation_end < ? ", @rend)
+    @max_product      = @success.order("max_price DESC").first
+    @max_bid_product  = @success.order("bids_count DESC").first
 
-    @watches  = Watch.where(created_at: rstart..rend)
-    @follows  = Follow.where(created_at: rstart..rend)
-    @searches = Search.where(created_at: rstart..rend)
+    @day = @rend.day
+
+    @users = User.where(@where_cr)
+
+    @bid_count = Bid.where(@where_cr).count
+    @bid_user  = Bid.where(@where_cr).distinct.count(:user_id)
+
+    @watches  = Watch.where(@where_cr)
+    @follows  = Follow.where(@where_cr)
+    @searches = Search.where(@where_cr)
+  end
+
+  def genres
+    @genres         = Genres.all.pluck(:id, :name).to_h
+    @product_counts = Product.where(template: nil).group(:genre_id).count
+    @bid_counts     = Bid.group(:genre_id).count
+    @watch_counts   = Watch.group(:genre_id).count
   end
 
   private
 
   def company_selectors
+    @company = params[:company]
+
     @company_selectors = User.companies.order(:id).map { |co| [co.company_remove_kabu, co.id] }
+  end
+
+  def date_selectors
+    @date = params[:date] ? Date.new(params[:date][:year].to_i, params[:date][:month].to_i, 1) : Date.today
+
+    @rstart = @date.beginning_of_month
+    @rend   = @date.end_of_month
+
+    @where_cr  = {created_at: @rstart..@rend}
+    @where_str = {dulation_start: @rstart..@rend}
+    @where_end = {dulation_end: @rstart..@rend}
   end
 end
