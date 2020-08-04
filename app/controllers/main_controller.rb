8e20ff843ef4abdeb1e4c3161965d13576d48886
川端洋平 <bata44883@gmail.com>
2020-08-04 14:23:50 +0900

    srcsetテスト

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,9 +1,5 @@
 class MainController < ApplicationController
   def index
-    # if Time.now < DateTime.parse("2018/05/15 00:00:00")
-    #   redirect_to "/lp/index.html"
-    # end
-
     @roots = Category.roots.order(:order_no)
 
     # 最近チェックした商品
