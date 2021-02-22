f6a5ffe30dc708f9950b4438c6941ff03da36449
ba-ta <bata44883@gmail.com>
2021-02-22 16:30:36 +0900

    jsonldエラー非出力

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -28,7 +28,7 @@ module ApplicationHelper
     jsonld = controller.render_to_string(formats: :jsonld)
     content_tag :script, jsonld.html_safe, type: Mime[:jsonld].to_s
   rescue ActionView::ActionViewError => e
-    logger.error e.message
+    # logger.error e.message
     nil
   ensure
      # render_to_string のバグ回避 https://github.com/rails/rails/issues/14173
