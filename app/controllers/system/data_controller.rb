7f0b2a91d0a1b1c6a73c597debc5694af7d907aa
ba-ta <bata44883@gmail.com>
2022-04-22 01:21:19 +0900

    vectors_batch_04

--- app/controllers/system/data_controller.rb
+++ app/controllers/system/data_controller.rb
@@ -166,6 +166,6 @@ class System::DataController < ApplicationController
 
   ### 画像ベクトル変換バッチ結果保存 ###
   def vectors_import
-    ProductNitamono.import([:product_id, :nitamono_id, :norm], params[:data])
+    ProductNitamono.import([:product_id, :nitamono_id, :norm], params[:_json])
   end
 end
